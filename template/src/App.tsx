b987d479960b7184afb605d6b0f409a974a9c293
warmachine7733 <prateekjena7733@gmail.com>
2025-02-11 14:42:28 +0530

    Added: styles for landing page

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import './App.css'
 
 const App = () => {
-  return <div>A webpack bootstrapped react cli</div>;
+  return (
+    <div className="container">
+      <div className="prompt-text">To make changes edit src/App.tsx</div>
+      <div className="logo-container">
+        <img className="logo" src="static/react.png" />
+        <img className="logo" src="static/webpack.png" />
+      </div>
+    </div>
+  );
 };
 
 export default App;
